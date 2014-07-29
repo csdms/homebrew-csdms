4041a8f3cbbbdbf97d8a4ee24821c342178a00cd
mcflugen <mcflugen@gmail.com>
2014-07-29 17:45:17 -0600

    Build with clang, not gcc.

--- babel.rb
+++ babel.rb
@@ -1,47 +1,36 @@
 require "formula"
 
-# Documentation: https://github.com/Homebrew/homebrew/wiki/Formula-Cookbook
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Babel < Formula
   version "1.4.0"
   homepage "http://computation.llnl.gov/casc/components"
   url "http://computation.llnl.gov/casc/components/docs/babel-1.4.0.tar.gz"
   sha1 "7dea34ace76b61ae76bf40e526734b1f23a7b11a"
 
-  # depends_on "cmake" => :build
-  depends_on :x11 # if your formula requires any X11/XQuartz components
+  option "with-check", "Run tests before installing"
+
+  depends_on :java
+  depends_on :python
   depends_on "libxml2"
   depends_on "chasm"
   depends_on "gcc"
-  #depends_on "homebrew/versions/gcc48"
-  depends_on "homebrew/python/scipy"
 
   def install
-    #ENV.deparallelize  # if your formula fails when building in parallel
-    ENV['JAVAPREFIX'] = "/Library/Java/JavaVirtualMachines/jdk1.8.0.jdk/Contents/Home"
-    ENV['CC'] = "gcc-4.8"
-    ENV['CXX'] = "g++-4.8"
+    ENV['JAVAPREFIX'] = "#{`/usr/libexec/java_home`.chomp}"
+
+    ENV['CC'] = "cc"
+    ENV['CXX'] = "c++"
     ENV['FC'] = "gfortran"
     ENV['F77'] = "gfortran"
+    ENV['CFLAGS'] = "-std=gnu89"
 
     system "./configure", "--prefix=#{prefix}", "--disable-documentation"
 
     system "make"
+    system "make check" if build.with? "check"
     system "make", "install"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test babel`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "#{bin}/babel", "--version"
   end
 end
