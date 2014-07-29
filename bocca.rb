0176b3098f90ffc497b3a1057afeb685e241db54
mcflugen <mcflugen@gmail.com>
2014-07-29 17:50:38 -0600

    Added depends on ccaffeine.

--- bocca.rb
+++ bocca.rb
@@ -1,36 +1,23 @@
 require "formula"
 
-# Documentation: https://github.com/Homebrew/homebrew/wiki/Formula-Cookbook
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Bocca < Formula
   homepage "http://www.cca-forum.org"
   url "http://www.cca-forum.org/download/cca-tools/cca-tools-latest/bocca-0.5.7.tar.gz"
   sha1 "9b996d3e4458d1b97713489a2ee5e1f9a270b12c"
 
-  # depends_on "cmake" => :build
-  depends_on :x11 # if your formula requires any X11/XQuartz components
-  depends_on "csdms/csdms/babel"
-  depends_on "csdms/csdms/cca-spec-babel"
+  depends_on :python
+  depends_on "babel"
+  depends_on "cca-spec-babel"
+  depends_on "ccaffeine"
+
+  skip_clean "lib"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
     system "./configure", "--prefix=#{prefix}"
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "install"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test bocca`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "#{bin}/bocca", "--version"
   end
 end
