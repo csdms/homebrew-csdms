1aa58e0d7b1d1e8e638d0e9f5c4959259f57e2ff
mcflugen <mcflugen@gmail.com>
2014-07-31 17:00:27 -0600

    Fixed brew test.

--- cca-spec-babel.rb
+++ cca-spec-babel.rb
@@ -5,16 +5,11 @@ class CcaSpecBabel < Formula
   url "http://www.cca-forum.org/download/cca-tools/cca-tools-latest/cca-spec-babel-0.8.6.tar.gz"
   sha1 "a149b96bd9945b2813d32467cafc9e7fe59d4248"
 
+  depends_on :fortran
   depends_on "libxml2"
-  depends_on "gcc"
   depends_on "babel"
 
   def install
-    ENV['CC'] = "cc"
-    ENV['CXX'] = "c++"
-    ENV['FC'] = "gfortran"
-    ENV['F77'] = "gfortran"
-
     system "./configure", "--disable-contrib",
       "--with-babel-config=#{HOMEBREW_PREFIX}/bin/babel-config",
       "--with-libxml2=/usr/local/opt/libxml2",
@@ -25,6 +20,6 @@ class CcaSpecBabel < Formula
   end
 
   test do
-    system "#{bin}/usr/local/bin/cca-spec-babel-config", "--version"
+    system "#{bin}/cca-spec-babel-config", "--version"
   end
 end
