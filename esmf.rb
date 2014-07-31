7e07df2fc71e41c6a238f05a78ba924568ab5175
mcflugen <mcflugen@gmail.com>
2014-07-31 17:40:09 -0600

    Removed unneeded comments.

--- esmf.rb
+++ esmf.rb
@@ -1,9 +1,5 @@
 require "formula"
 
-# Documentation: https://github.com/Homebrew/homebrew/wiki/Formula-Cookbook
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Esmf < Formula
   version "6.3.0"
   homepage "http://www.earthsystemmodeling.org"
@@ -12,12 +8,11 @@ class Esmf < Formula
 
   option "with-check", "Run tests before installing"
 
-  # depends_on "cmake" => :build
-  depends_on :x11 # if your formula requires any X11/XQuartz components
   depends_on :fortran
 
   def install
-    ENV.deparallelize  # if your formula fails when building in parallel
+    ENV.deparallelize
+
     ENV['ESMF_CXX'] = "#{ENV.cc}"
     ENV['ESMF_F90'] = "#{ENV.fc}"
     ENV['ESMF_COMM'] = "mpiuni"
@@ -32,15 +27,6 @@ class Esmf < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test esmf`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
