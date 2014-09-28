3d196a500658e73c391e9937c2df48b57dfaf6d5
mcflugen <huttone@colorado.edu>
2014-09-27 23:10:53 -0600

    Added options to give python executable for linux.

--- bocca.rb
+++ bocca.rb
@@ -5,7 +5,9 @@ class Bocca < Formula
   url "http://www.cca-forum.org/download/cca-tools/cca-tools-latest/bocca-0.5.7.tar.gz"
   sha1 "9b996d3e4458d1b97713489a2ee5e1f9a270b12c"
 
-  depends_on :python
+  option "with-python=", "Path to a python binary" if OS.linux?
+
+  depends_on :python unless OS.linux?
   depends_on "babel"
   depends_on "cca-spec-babel"
   depends_on "ccaffeine"
@@ -13,6 +15,8 @@ class Bocca < Formula
   skip_clean "lib"
 
   def install
+    ENV.prepend_path 'PATH', File.dirname(which_babel_python)
+
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
   end
@@ -20,4 +24,17 @@ class Bocca < Formula
   test do
     system "#{bin}/bocca", "--version"
   end
+
+  def which_babel_python
+    python = `babel-config --query-var=WHICH_PYTHON`.strip
+    raise "python not found" unless File.exist? python
+    return python
+  end
+
+  def which_python
+    python = ARGV.value('with-python') || which('python').to_s
+    raise "#{python} not found" unless File.exist? python
+    return python
+  end
+
 end
