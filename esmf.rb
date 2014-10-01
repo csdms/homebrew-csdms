59a03220757d66854b67b79d48a6a1b896bc4c00
mcflugen <huttone@colorado.edu>
2014-10-01 15:36:29 -0600

    Added enable-esmpy option to install with ESMPY.

--- esmf.rb
+++ esmf.rb
@@ -7,6 +7,8 @@ class Esmf < Formula
   sha1 "f459a65373fd5a7925ea935bc75b66283b27936b"
 
   option "with-check", "Run tests before installing"
+  option "enable-esmpy", "Build ESMPy"
+  option "with-python=", "Path to a python binary" if OS.linux?
 
   depends_on :fortran
   depends_on "csdms/dupes/netcdf"
@@ -24,12 +26,44 @@ class Esmf < Formula
     ENV['ESMF_COMM'] = "mpiuni"
     ENV['ESMF_DIR'] = buildpath
     ENV['ESMF_INSTALL_PREFIX'] = prefix
-    ENV['ESMF_COMPILER'] = File.basename("#{ENV.fc}")
-    ENV['ESMF_COMPILER'] += File.basename("#{ENV.cc}") if OS.mac?
+    ENV['ESMF_COMPILER'] = esmf_compiler
 
     system "make"
     system "make check" if build.with? "check"
     system "make", "install"
+
+    if build.include? 'enable-esmpy'
+      ENV.prepend_path 'PATH', File.dirname(which_python)
+
+      cd "src/addon/ESMPy" do
+        system "python", "setup.py", "build", "--ESMFMKFILE=" + which_esmf_mk,
+          "install", "--prefix=#{prefix}"
+
+        system "python", "setup.py", "test_all" if build.with? "check"
+      end
+    end
+
+  end
+
+  def esmf_compiler
+    compiler = File.basename("#{ENV.fc}")
+    compiler += File.basename("#{ENV.cc}") if OS.mac?
+    return compiler
+  end
+
+  def which_python
+    python = ARGV.value('with-python') || which('python').to_s
+    raise "#{python} not found" unless File.exist? python
+    return python
+  end
+
+  def which_esmf_mk
+    arch = if OS.mac? then "Darwin" else "Linux" end
+
+    path_to_file = lib +
+      "libO/#{arch}.#{esmf_compiler}.64.mpiuni.default/esmf.mk"
+    raise "#{path_to_file} not found" unless File.exist? path_to_file
+    return path_to_file
   end
 
   test do
